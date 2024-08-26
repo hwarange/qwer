a5444ff23d193ac5c87f21e1feabfc4dc57213e8
hwarange <asdf78964@naver.com>
2024-08-26 15:47:25 +0900

    넘파이 마무리

--- Ex.py
+++ Ex.py
@@ -3,3 +3,19 @@ t = int(input())
 for i in range(t):
     a, b, c = map(int, input().split())
     print(f'{c%a}{c//a:02}')
+
+\
+count = int(results[i]['DT'])
+    region = results[i]['C1_NM']
+
+    formatted_date = f"{PRD_DE[:4]}-{PRD_DE[4:6]}-01"
+
+    if PRD_DE in target:
+        PRD_DE = int(PRD_DE[:4])
+    
+    if PRD_DE == 2015 :
+        year[0] = 2015
+        year.append(count)
+        if len(year) == 
+
+print(len(year))
